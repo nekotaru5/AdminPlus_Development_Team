c5c071c852f06c67155d88aba139d04797c24843
nekotaru5 <noobkun.jp@gmail.com>
2025-05-28 02:25:29 +0900

    main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def on_ready():
 
 @bot.command()
 async def Admin(ctx):
-    await ctx.send('( ˙꒳​˙ )))ｳﾝｳﾝ!!それで?それで?なんか用?')
+    await ctx.send('( ˙꒳​˙ )))ｳﾝｳﾝ!!それで?それで?なんか用???')
 
 # ホワイトリスト管理コマンド
 @bot.tree.command(name="add_whitelist", description="コマンド許可ロールを追加します")
